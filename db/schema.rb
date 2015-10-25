3a6216c6b6bd157f3bf735ec4530579826e9c2f3
Valeskyta <valeska.arquitecto@gmail.com>
2015-10-25 20:23:21 -0300

    ralations between tables users products reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025170502) do
+ActiveRecord::Schema.define(version: 20151025222631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,16 +24,21 @@ ActiveRecord::Schema.define(version: 20151025170502) do
     t.string   "photo"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.text     "review"
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -53,5 +58,7 @@ ActiveRecord::Schema.define(version: 20151025170502) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "products", "users"
   add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
